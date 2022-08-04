36f55f9efcf4f8bbce854e613f4547f595c55bb3
unknown <abkansu@gmail.com>
2022-08-04 16:34:23 +0300

    revert

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -6,9 +6,6 @@ import com.thoughtworks.xstream.io.xml.StaxDriver;
 
 public class Main {
     public static void main(String[] args) {
-        XStream xStream = new XStream(new StaxDriver()){
 
-
-        };
     }
 }
